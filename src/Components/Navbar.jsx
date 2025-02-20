6ee18319f200293b9cbbbe26570299cb2a496908
NARENDRA TOMAR <140541020+narendratomar45@users.noreply.github.com>
2025-02-20 16:14:30 +0530

    Updated Navbar

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Navbar = () => {
   return (
-    <div className=" flex justify-between p-5 bg-blue-950 text-white">
+    <div className=" flex justify-between p-5 bg-blue-950 text-yellow-900">
       <div>Dev Tinder</div>
       <div className=" w-10 h-10 border-2 border-red-500 rounded-full p-2">
         Profile
