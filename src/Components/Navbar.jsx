8bb64045048160c6aef4537b62a0c76792ae6868
NARENDRA TOMAR <140541020+narendratomar45@users.noreply.github.com>
2025-02-20 16:02:39 +0530

    install tailwind + add Navbar

---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <div className=" flex justify-between p-5 bg-blue-950 text-white">
+      <div>Dev Tinder</div>
+      <div>Profile</div>
+    </div>
+  );
+};
+
+export default Navbar;
